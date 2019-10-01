8cafa92217c42c5afceee38f0b6367d9917da833
Kyle Bashour <kylebshr@me.com>
2019-09-30 23:17:14 -0700

    Initial commit

---
+++ Recipes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Recipes
+//
+//  Created by Kyle Bashour on 9/30/19.
+//  Copyright © 2019 Kyle Bashour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
